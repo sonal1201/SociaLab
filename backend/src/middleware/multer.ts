1a31fecb353150a74e82177555e55d4a650b6627
Sonal Singh <sonal1201kumar@gmail.com>
2025-11-23 20:50:22 +0530

    multerfix

--- backend/src/middleware/multer.ts
+++ backend/src/middleware/multer.ts
@@ -1,14 +1,23 @@
-import multer from "multer"
-
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./public") //file to keep image
-    },
+  destination: (req, file, cb) => {
+    const uploadPath = path.join(__dirname, "../public");
 
-    filename: (req, file, cb) => {
-        cb(null, file.originalname) // name of the file
+    // Create folder if not exists
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
     }
-})
 
-export const upload = multer({ storage })
+    cb(null, uploadPath);
+  },
+
+  filename: (req, file, cb) => {
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, unique + "-" + file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
